63c5fbc85ab503a10dca6d1d6e72df022cbf346b
sadbrain <riotgheptrannhulol@gmail.com>
2024-04-18 13:02:05 +0700

    feature: write api for login function, write code createNewToken function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,5 +10,36 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends ApiController
 {  
-    
+    public function login(Request $request){
+    	$validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|min:6|max:100',
+            'password' => 'required|string|min:6|max:255|regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W)(?!.*\s).*$/',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (!$token = JWTAuth::attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->createNewToken($token);
+    }
+
+    public function refresh() {
+        return $this->createNewToken(auth("api")->refresh());
+    }
+
+    protected function createNewToken($token){
+        auth()->user()->company;
+        auth()->user()->role;
+        
+        return response()->json([   
+            'access_token' => (string) $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60,
+            'user' => auth()->user()
+            
+        ]);
+    }
 }
